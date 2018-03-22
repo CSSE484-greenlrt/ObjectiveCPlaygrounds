d9891a2bb41ca95f13a9d456bad9b5c0f8367fa0
Ryan Greenlee <Ryan@kimberlys-mbp.wlan.rose-hulman.edu>
2018-03-22 12:15:07 -0400

    Worked on Objective-C Playgrounds.

--- Objective-C Playgrounds/Objective-C Playgrounds/Variables.h
+++ Objective-C Playgrounds/Objective-C Playgrounds/Variables.h
@@ -6,9 +6,12 @@
 //  Copyright (c) 2015 Rose-Hulman. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface Variables : NSObject
+//{
+//    int myInstanceVariable;
+//}
 
 // Class Method
 + (void) run;
@@ -17,6 +20,11 @@
 
 
 // Properties
+//@property (nonatomic, weak) NSNumber* myNumber;
+@property (nonatomic, strong) NSNumber* myNumber;
+@property (nonatomic, copy) NSString* myString;
+@property (nonatomic) int myInt;
+
 
 
 @end
--- Objective-C Playgrounds/Objective-C Playgrounds/Variables.m
+++ Objective-C Playgrounds/Objective-C Playgrounds/Variables.m
@@ -9,10 +9,16 @@
 #import "Variables.h"
 
 // Constants
+const int myConstantInt = 7;
+#define  myConstantDefine 8
+NSString* const myConstantStr = @"Hello";
+#define myConstantString @"World"
 
 
 // Internal property
-
+@interface Variables ()
+@property (nonatomic) int myInternalInt;
+@end
 
 @implementation Variables
 
@@ -21,25 +27,47 @@
     NSLog(@"--- Variables ---");
 
     // Primitives
-
-
+    int x = 7;
+    double d = 1.234;
+    NSLog(@"x = %d  d = %f", x, d);
 
     // Objects (note the * symbol)
-
-
+    NSString* myStr = @"Hello World";
+    NSNumber* verboseNumber =[[NSNumber alloc] initWithInt:8];
+    NSNumber* shorthandNumber = @9;
+    NSLog(@"myStr = %@  number = %@ and %@", myStr, verboseNumber, shorthandNumber);
 
     // Constants
-
-
-
+    NSLog(@"numbers = %d and %d Strings = %@ and %@",
+          myConstantInt, myConstantDefine,
+          myConstantStr, myConstantString);
 
     // Sneaky primitives (note the LACK of a * symbol)
-
-
-
+    NSInteger y = 10;
+    BOOL b = YES;
+    CGFloat z = 2.345;
+    CGRect rect = CGRectMake(0, 0, 100.0, 100.0);
+    NSLog(@"b = %d y = %d   \nz = %f width = %f",
+          b, (int)y, z, rect.size.width);
 
     // Properties
+    Variables* obj = [[Variables alloc] init];
+    obj.myNumber = @10;
+    obj.myString = @"We share public properties!";
+    obj.myInt = 42;
+    NSLog(@"myNumber = %@   myString = %@   myInt = %d",
+          obj.myNumber, obj.myString, obj.myInt);
+    [obj exampleInstanceMethod];
+    NSLog(@"myNumber = %@   myString = %@   myInt = %d",
+          obj.myNumber, obj.myString, obj.myInt);
+    
+}
 
+- (void) exampleInstanceMethod {
+    self.myNumber = @200;
+    self.myString = @"We don't share private properties!";
+    self.myInt = 420;
+    self.myInternalInt = 222;
 }
 
 @end
