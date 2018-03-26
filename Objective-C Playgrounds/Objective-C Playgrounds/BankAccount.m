ef0c3430b7e084d7dd4f0f1c7a4a873c5e1676bb
Ryan Greenlee <Ryan@kimberlys-mbp.wlan.rose-hulman.edu>
2018-03-26 12:24:27 -0400

    Added the classes

---
+++ Objective-C Playgrounds/Objective-C Playgrounds/BankAccount.m
@@ -0,0 +1,33 @@
+//
+//  BankAccount.m
+//  Objective-C Playgrounds
+//
+//  Created by Ryan Greenlee on 3/26/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+#import "BankAccount.h"
+
+@implementation BankAccount
+
+- (id) initWithName:(NSString*) name withBalance:(double) balance {
+    self = [super init];
+    if (self) {
+        self.name = name;
+        self.balance = balance;
+    }
+    return self;
+}
+- (void) deposit:(double) amount {
+    self.balance += amount;
+}
+
+- (void) withdraw:(double) amount {
+    self.balance -= amount;
+}
+
+- (NSString*) description {
+    return [NSString stringWithFormat:@"Name: %@ Balance: $%.f", self.name, self.balance];
+}
+
+@end
---
+++ Tic-Tac-Toe/Tic-Tac-Toe/ObjCTicTacToeGame.h
@@ -0,0 +1,35 @@
+//
+//  ObjCTicTacToeGame.h
+//  Tic-Tac-Toe
+//
+//  Created by Ryan Greenlee on 3/26/18.
+//  Copyright © 2018 David Fisher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, GameState) {
+    GameStateXTurn,
+    GameStateOTurn,
+    GameStateXWon,
+    GameStateOWon,
+    GameStateTie
+};
+
+typedef NS_ENUM(NSInteger, MarkType) {
+    MarkTypeNone,
+    MarkTypeX,
+    MarkTypeO
+};
+
+@interface ObjCTicTacToeGame : NSObject {
+    MarkType gameBoard[9];
+}
+
+- (NSString*) getGameStateString;
+- (NSString*) getMarkTypeString:(MarkType) mark;
+- (void) pressedSquare:(NSInteger) index;
+
+@property (nonatomic) GameState gameState;
+
+@end
